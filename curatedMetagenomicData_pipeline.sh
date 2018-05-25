1b68968445e938c25b8cb707ca509727d57b0c4b
Ubuntu <ubuntu@ip-172-31-75-69.ec2.internal>
2018-05-25 20:14:02 +0000

    commented out shell script for debugging

--- curatedMetagenomicData_pipeline.sh
+++ curatedMetagenomicData_pipeline.sh
@@ -40,20 +40,20 @@ echo 'Downloaded.'
 echo 'Concatenating runs...'
 cat ${sample}/reads/*.fastq > ${sample}/reads/${sample}.fastq
 
-mkdir -p ${sample}/humann2
-humann2 --input ${sample}/reads/${sample}.fastq --output ${sample}/humann2 --nucleotide-database ${pc} --protein-database ${pp} --threads=${ncores}
-humann2_renorm_table --input ${sample}/humann2/${sample}_genefamilies.tsv --output ${sample}/humann2/${sample}_genefamilies_relab.tsv --units relab
-humann2_renorm_table --input ${sample}/humann2/${sample}_pathabundance.tsv --output ${sample}/humann2/${sample}_pathabundance_relab.tsv --units relab
-python run_markers2.py --input_dir ${sample}/humann2/${sample}_humann2_temp/ --bt2_ext _metaphlan_bowtie2.txt --metaphlan_path ${pm} --metaphlan_db ${pmdb} --output_dir ${sample}/humann2 --nprocs ${ncores}
+#mkdir -p ${sample}/humann2
+#humann2 --input ${sample}/reads/${sample}.fastq --output ${sample}/humann2 --nucleotide-database ${pc} --protein-database ${pp} --threads=${ncores}
+#humann2_renorm_table --input ${sample}/humann2/${sample}_genefamilies.tsv --output ${sample}/humann2/${sample}_genefamilies_relab.tsv --units relab
+#humann2_renorm_table --input ${sample}/humann2/${sample}_pathabundance.tsv --output ${sample}/humann2/${sample}_pathabundance_relab.tsv --units relab
+#python run_markers2.py --input_dir ${sample}/humann2/${sample}_humann2_temp/ --bt2_ext _metaphlan_bowtie2.txt --metaphlan_path ${pm} --metaphlan_db ${pmdb} --output_dir ${sample}/humann2 --nprocs ${ncores}
 
-mkdir genefamilies; mv ${sample}/humann2/${sample}_genefamilies.tsv genefamilies/${sample}.tsv;
-mkdir genefamilies_relab; mv ${sample}/humann2/${sample}_genefamilies_relab.tsv genefamilies_relab/${sample}.tsv;
-mkdir marker_abundance; mv ${sample}/humann2/${sample}.marker_ab_table marker_abundance/${sample}.tsv;
-mkdir marker_presence; mv ${sample}/humann2/${sample}.marker_pres_table marker_presence/${sample}.tsv;
-mkdir metaphlan_bugs_list; mv ${sample}/humann2/${sample}_humann2_temp/${sample}_metaphlan_bugs_list.tsv metaphlan_bugs_list/${sample}.tsv;
-mkdir pathabundance; mv ${sample}/humann2/${sample}_pathabundance.tsv pathabundance/${sample}.tsv;
-mkdir pathabundance_relab; mv ${sample}/humann2/${sample}_pathabundance_relab.tsv pathabundance_relab/${sample}.tsv;
-mkdir pathcoverage; mv ${sample}/humann2/${sample}_pathcoverage.tsv pathcoverage/${sample}.tsv;
-mkdir humann2_temp; mv ${sample}/humann2/${sample}_humann2_temp/ humann2_temp/ #comment this line if you don't want to keep humann2 temporary files
+#mkdir genefamilies; mv ${sample}/humann2/${sample}_genefamilies.tsv genefamilies/${sample}.tsv;
+#mkdir genefamilies_relab; mv ${sample}/humann2/${sample}_genefamilies_relab.tsv genefamilies_relab/${sample}.tsv;
+#mkdir marker_abundance; mv ${sample}/humann2/${sample}.marker_ab_table marker_abundance/${sample}.tsv;
+#mkdir marker_presence; mv ${sample}/humann2/${sample}.marker_pres_table marker_presence/${sample}.tsv;
+#mkdir metaphlan_bugs_list; mv ${sample}/humann2/${sample}_humann2_temp/${sample}_metaphlan_bugs_list.tsv metaphlan_bugs_list/${sample}.tsv;
+#mkdir pathabundance; mv ${sample}/humann2/${sample}_pathabundance.tsv pathabundance/${sample}.tsv;
+#mkdir pathabundance_relab; mv ${sample}/humann2/${sample}_pathabundance_relab.tsv pathabundance_relab/${sample}.tsv;
+#mkdir pathcoverage; mv ${sample}/humann2/${sample}_pathcoverage.tsv pathcoverage/${sample}.tsv;
+#mkdir humann2_temp; mv ${sample}/humann2/${sample}_humann2_temp/ humann2_temp/ #comment this line if you don't want to keep humann2 temporary files
 
-rm -r ${sample}
+#rm -r ${sample}
