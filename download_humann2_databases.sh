db12d4a07a5e3919b0a38ba7c9751ecbab3d6761
Levi Waldron <lwaldron.research@gmail.com>
2018-05-25 16:42:37 -0400

    commented-out options for full databases

--- download_humann2_databases.sh
+++ download_humann2_databases.sh
@@ -6,7 +6,14 @@
 
 source configrc
 pip install humann2==$humann2_version
+
 mkdir databases
-humann2_databases --download chocophlan DEMO databases/humann2_database_downloads_$humann2_version
-humann2_databases --download uniref DEMO_diamond databases/humann2_database_downloads_$humann2_version
+
+humann2_databases --download chocophlan DEMO databases/humann2_database_downloads_$humann2_version #DEMO
+# humann2_databases --download chocophlan full databases/humann2_database_downloads_$humann2_version #FULL
+
+humann2_databases --download uniref DEMO_diamond databases/humann2_database_downloads_$humann2_version #DEMO
+# humann2_databases --download uniref uniref90_diamond databases/humann2_database_downloads_$humann2_version #FULL
+
+aws s3 rm --recursive s3://curatedmetagenomics.bioconductor.org/humann2_database_downloads_$humann2_version
 aws s3 cp --recursive databases s3://curatedmetagenomics.bioconductor.org/
