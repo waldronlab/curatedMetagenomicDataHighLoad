4c1f3d0acaf1cd0b0b75ba7235d61f729915bc41
Francesco Beghini <francesco.beghini@unitn.it>
2020-04-27 11:19:57 +0200

    HUMAnN2 is now just HUMAnN

--- docker/curatedMetagenomics/curatedMetagenomicData_pipeline.sh
+++ docker/curatedMetagenomics/curatedMetagenomicData_pipeline.sh
@@ -12,8 +12,8 @@ sample=$1
 runs=$2
 
 ### before running this script, set these paths and variables
-pc=/dbs/humann2/chocophlan # chocophlan database (nucleotide-database for humann2, like /databases/chocophlan
-pp=/dbs/humann2/uniref # uniref database (protein-database for humann2, like /databases/uniref)
+pc=/dbs/humann/chocophlan # chocophlan database (nucleotide-database for humann, like /databases/chocophlan
+pp=/dbs/humann/uniref # uniref database (protein-database for humann, like /databases/uniref)
 pmdb=/opt/metaphlan2/biobakery-metaphlan2/db_v30_CHOCOPhlAn_201901/mpa_v30_CHOCOPhlAn_201901.pkl #metaphlan2 database (like /tools/metaphlan2/db_v20/mpa_v20_m200.pkl)
 ncores=2 #number of cores
 
@@ -22,12 +22,12 @@ mkdir -p $pp
 
 if [ ! "$(ls -A $pp)" ]; then
     wget https://storage.googleapis.com/curatedmetagenomicdata/dbs/uniref/uniref90_annotated_1_1.tar.gz
-    tar -xvz -C /dbs/humann2/uniref/ -f uniref90_annotated_1_1.tar.gz
+    tar -xvz -C /dbs/humann/uniref/ -f uniref90_annotated_1_1.tar.gz
 fi
 
 if [ ! "$(ls -A $pc)" ]; then
     wget https://storage.googleapis.com/curatedmetagenomicdata/dbs/chocophlan/full_chocophlan_plus_viral.v0.1.1.tar.gz  
-    tar -xvz -C /dbs/humann2/chocophlan/ -f full_chocophlan_plus_viral.v0.1.1.tar.gz
+    tar -xvz -C /dbs/humann/chocophlan/ -f full_chocophlan_plus_viral.v0.1.1.tar.gz
 fi
 
 
@@ -55,29 +55,29 @@ echo 'Downloaded.'
 echo 'Concatenating runs...'
 cat reads/*.fastq > reads/${sample}.fastq
 
-mkdir -p humann2
-echo 'Running humann2'
-humann2 --input reads/${sample}.fastq --output humann2 --nucleotide-database ${pc} --protein-database ${pp} --threads=${ncores}
+mkdir -p humann
+echo 'Running humann'
+humann --input reads/${sample}.fastq --output humann --nucleotide-database ${pc} --protein-database ${pp} --threads=${ncores}
 echo 'renorm_table runs'
-humann2_renorm_table --input humann2/${sample}_genefamilies.tsv --output humann2/${sample}_genefamilies_relab.tsv --units relab
-humann2_renorm_table --input humann2/${sample}_pathabundance.tsv --output humann2/${sample}_pathabundance_relab.tsv --units relab
+humann_renorm_table --input humann/${sample}_genefamilies.tsv --output humann/${sample}_genefamilies_relab.tsv --units relab
+humann_renorm_table --input humann/${sample}_pathabundance.tsv --output humann/${sample}_pathabundance_relab.tsv --units relab
 echo 'run_markers2.py'
 # NOTE: using absolute path here!!!
 python /root/run_markers2.py \
-    --input_dir humann2/${sample}_humann2_temp/ \
+    --input_dir humann/${sample}_humann_temp/ \
     --bt2_ext _metaphlan_bowtie2.txt \
     --metaphlan_db ${pmdb} \
-    --output_dir humann2 \
+    --output_dir humann \
     --nprocs ${ncores}
 
-mkdir genefamilies; mv humann2/${sample}_genefamilies.tsv genefamilies/${sample}.tsv;
-mkdir genefamilies_relab; mv humann2/${sample}_genefamilies_relab.tsv genefamilies_relab/${sample}.tsv;
-mkdir marker_abundance; mv humann2/${sample}.marker_ab_table marker_abundance/${sample}.tsv;
-mkdir marker_presence; mv humann2/${sample}.marker_pres_table marker_presence/${sample}.tsv;
-mkdir metaphlan_bugs_list; mv humann2/${sample}_humann2_temp/${sample}_metaphlan_bugs_list.tsv metaphlan_bugs_list/${sample}.tsv;
-mkdir pathabundance; mv humann2/${sample}_pathabundance.tsv pathabundance/${sample}.tsv;
-mkdir pathabundance_relab; mv humann2/${sample}_pathabundance_relab.tsv pathabundance_relab/${sample}.tsv;
-mkdir pathcoverage; mv humann2/${sample}_pathcoverage.tsv pathcoverage/${sample}.tsv;
-mkdir humann2_temp; mv humann2/${sample}_humann2_temp/ humann2_temp/ #comment this line if you don't want to keep humann2 temporary files
+mkdir genefamilies; mv humann/${sample}_genefamilies.tsv genefamilies/${sample}.tsv;
+mkdir genefamilies_relab; mv humann/${sample}_genefamilies_relab.tsv genefamilies_relab/${sample}.tsv;
+mkdir marker_abundance; mv humann/${sample}.marker_ab_table marker_abundance/${sample}.tsv;
+mkdir marker_presence; mv humann/${sample}.marker_pres_table marker_presence/${sample}.tsv;
+mkdir metaphlan_bugs_list; mv humann/${sample}_humann_temp/${sample}_metaphlan_bugs_list.tsv metaphlan_bugs_list/${sample}.tsv;
+mkdir pathabundance; mv humann/${sample}_pathabundance.tsv pathabundance/${sample}.tsv;
+mkdir pathabundance_relab; mv humann/${sample}_pathabundance_relab.tsv pathabundance_relab/${sample}.tsv;
+mkdir pathcoverage; mv humann/${sample}_pathcoverage.tsv pathcoverage/${sample}.tsv;
+mkdir humann_temp; mv humann/${sample}_humann_temp/ humann_temp/ #comment this line if you don't want to keep humann2 temporary files
 
 rm -r reads
